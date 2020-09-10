e90ee21999354b6add837cdcc1611ed84564621a
Joshua Fung <formygit@gmail.com>
2020-09-10 16:16:38 -0700

    Added in relavent courses. Looks ugly af, fix it

--- src/components/courses.component.js
+++ src/components/courses.component.js
@@ -8,9 +8,27 @@ export default class Courses extends Component{
         return(
             <div id="coursesSection" className="sectionPositioning" >
                 <Card id="courseCardPositioning">
-                    <Card.Header>Relevant Coursework</Card.Header>
+                    <Card.Header><strong>Dalhousie University</strong><p>Computer Science B.S</p></Card.Header>
                     <Card.Body>
-                        <Card.Text>List of courses</Card.Text>
+                        <h4>Relavent Course Work</h4>
+                        <br/>
+                        <section id="coursesTextSection">
+                            <Card.Text>> Intro to Computer Programming</Card.Text>
+                            <Card.Text>> Intro to Computer Systems</Card.Text>
+                            <Card.Text>> Intro to Web Design & Development</Card.Text>
+                            <Card.Text>> Intro to Computer Science</Card.Text>
+                            <Card.Text>> Data Structures & Algorithms</Card.Text>
+                            <Card.Text>> Discrete Structures I</Card.Text>
+                            <Card.Text>> Systems Programming</Card.Text>
+                            <Card.Text>> Software Development</Card.Text>
+                            <Card.Text>> Intro to Database Systems</Card.Text>
+                            <Card.Text>> Intro to Server Side Scripting</Card.Text>
+                            <Card.Text>> Introduction to Info Security</Card.Text>
+                            <Card.Text>> Social/Ethical/Proffesional Issuse in Computer Science</Card.Text>
+                            <Card.Text>> Operating Systems</Card.Text>
+                            <Card.Text>> Designing User Interfaces</Card.Text>
+                            <Card.Text>> Network Computing</Card.Text>
+                        </section>
                     </Card.Body>
                 </Card>
             </div>
